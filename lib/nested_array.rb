180cef3fa2b12d8404e42de14eaaca727bf0cd0b
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 19:51:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,4 +1,5 @@
 # Examples inspired by U. S. National Organic Standards
+require "pry"
 
 ORGANIC_PRODUCE = [
   "Strawberries",
@@ -21,21 +22,41 @@ def assembled_matrix
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
+  
+  storage_room = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+  storage_room
+  
 end
 
 def sorted_matrix
   # Using Array literal syntax only, build another nested array that 
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
+  
+  sorted_conventional = CONVENTIONAL_PRODUCE.sort
+  sorted_organic = ORGANIC_PRODUCE.sort 
+  
+  sorted_storage = [sorted_conventional, sorted_organic]
+  sorted_storage
+  
 end
 
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  
+  
+  lookup = matrix[row][column]
+  lookup
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
+  
+  result = matrix[row][column] << new_value
+  result
+  
 end
