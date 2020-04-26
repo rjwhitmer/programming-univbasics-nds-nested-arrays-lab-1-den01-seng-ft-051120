9a0b02b8dc7ae70015714033e71a7351891c258f
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-26 19:52:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -56,7 +56,7 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
-  result = matrix[row][column] << new_value
+  result = matrix[row] << new_value
   result
   
 end
